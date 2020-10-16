f557544511679895fee5af71fe7857819b5a9a8c
Guangshuo CHEN <guangshuo.chen@inria.fr>
2020-10-16 08:24:51 +0800

    example5 and homework

---
+++ homework1/container/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+model = None
+
+app = Flask(__name__)
+
+
+@app.route("/model", methods=['POST'])
+def call():
+    global model
+    payload = request.json
+    # TODO: prepare you payload
+
+    if model is None:
+        # TODO: load your model
+        model = 'TODO'
+
+    # TODO: call your model and receive the resule
+    return jsonify({'predict': 'TODO')})
+
+
+if __name__ == "__main__":
+    app.run(port="8080")
